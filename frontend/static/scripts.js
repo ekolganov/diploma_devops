c05d7a17377795c8353f948c7fbc0b2a338c54ed
karamel32 <29039986+karamel32@users.noreply.github.com>
2021-12-17 08:54:11 +0300

    FE BE before ending

--- frontend/static/scripts.js
+++ frontend/static/scripts.js
@@ -14,9 +14,18 @@ function send_data(be_uri) {
         data: JSON.stringify(server_data),
         contentType: "application/json",
         dataType: 'json',
-            success: function(result) {
-                console.log("Result:");
-                console.log(result);
-            }
+
+        success: function(result) {
+            console.log("Result:");
+            console.log(result.status_recieved_data);
+            $("#mytable").remove();
+            var table = '<table id="mytable" <tr><th width="20%" >Game date</th><th width="30%">Home - Away teams</th><th width="10%">Home - Away score</th><th width="40%">1st, 2nd, 3d game stars</th></tr ></table>';
+            $('body').append(table);
+            var trHTML = '';
+            $.each(result, function (i, table) {
+            trHTML += '<tr><td width="20%" align="center">' + table.date + '</td><td width="30%" align="center">' + table.teams + '</td><td width="10%" align="center">' + table.score + '</td><td width="40%" align="center">' + table.stars + '</td></tr>';
+            });
+            $('#mytable').append(trHTML);
+        }
     });
 }
