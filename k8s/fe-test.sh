eb09b8ba02824c79349efd30ac93b7106e040ae9
root <root@debian11.WORKGROUP>
2022-01-15 19:34:29 +0300

    try Jenkinsfile

--- k8s/fe-test.sh
+++ k8s/fe-test.sh
@@ -5,11 +5,13 @@ echo "SET VERSION IMAGE !!!!!!"
 exit 1
 fi
 
+deploy_file=../k8s/fe-deployment-nhl_app.yaml
+
 source ~/.bashrc
 cd ~/diploma_devops/frontend
 #kubectl delete -f nhl_app_fe.yaml
 docker build -t karamel32/nhl_app:$1 .
 docker push karamel32/nhl_app:$1
-sed -i "s/nhl_app:[0-9]\+\.[0-9]\+\.[0-9]\+/nhl_app:$1/g" deployment-nhl_app_fe.yaml
-kubectl apply -f deployment-nhl_app_fe.yaml
+sed -i "s/nhl_app:[0-9]\+\.[0-9]\+\.[0-9]\+/nhl_app:$1/g" $deploy_file
+kubectl apply -f $deploy_file
 kubectl get po -w
